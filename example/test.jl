db42ea0423d596c10fa92c5efb758b7e78a9187a
ZhixuPang <137134445+ZhixuPang@users.noreply.github.com>
2024-05-02 21:52:58 +0800

    Update test.jl

--- example/test.jl
+++ example/test.jl
@@ -1,20 +1,14 @@
-# 引入所需的包
-using Pkg
-# using ArgParse
-using CSV
-using DataFrames
-using DelimitedFiles
-using Base.Threads
+using Pkg, CSV, DataFrames, DelimitedFiles, Base.Threads
 
 include("../src/MIBLUP.jl")
 bfile = "data"
 println("Start read plink")
 data = MIBLUP.read_plink(bfile)
 #covar = readdlm("covar.txt", Float64)
-# 进行基因型插补
+
 #geno = MIBLUP.geno_imputation(data.bed)
 geno = data.bed
-# 获取y向量
+
 pheno = CSV.read("pheno.txt", DataFrame)
 y = Vector(pheno[:, 1])
 
